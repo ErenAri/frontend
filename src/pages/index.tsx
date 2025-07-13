f334eb90191743c28734b6bdb5c7f355dc6064a4
ErenAri <erenari27@gmail.com>
2025-07-13 22:31:07 +0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
     setLoading(true);
     setError("");
     try {
-      const res = await fetch("http://localhost:8000/predict", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ symbol, days: 365 })
